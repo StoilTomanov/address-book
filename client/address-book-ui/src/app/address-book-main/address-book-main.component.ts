c9465ec3aa9d7538fffc0740b322346824fa262c
StoilTomanov <stoiltomanov@gmail.com>
2024-09-29 00:51:54 +0300

    add address book table

--- client/address-book-ui/src/app/address-book-main/address-book-main.component.ts
+++ client/address-book-ui/src/app/address-book-main/address-book-main.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AddressRow } from '../models/address-book';
 
 @Component({
-  selector: 'app-address-book-main',
-  templateUrl: './address-book-main.component.html',
-  styleUrls: ['./address-book-main.component.scss']
+    selector: 'app-address-book-main',
+    templateUrl: './address-book-main.component.html',
+    styleUrls: ['./address-book-main.component.scss'],
 })
 export class AddressBookMainComponent {
+    rows: AddressRow[] = [
+        { id: 123, name: 'John', phone: '+359 885 745 458', email: 'john.doe@email.com', expanded: false },
+        { id: 456, name: 'Sara', phone: '+359 456 123 753', email: 'sara.doe@email.com', expanded: true },
+    ];
 
+    toggleRowDetails(row: AddressRow): void {
+        this.rows.map((rowInRows) => {
+            if (row.id === rowInRows.id) {
+                row.expanded = !row.expanded;
+            } else {
+                rowInRows.expanded = false;
+            }
+        });
+    }
 }
---
+++ client/address-book-ui/src/app/models/address-book.ts
@@ -0,0 +1,12 @@
+export interface AddressRow {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    address?: string;
+    jobRole?: string;
+    linkedIn?: string;
+    birthday?: string;
+    notes?: string;
+    expanded: boolean;
+}
