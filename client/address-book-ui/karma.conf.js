85562e37afa4f4ae395a96a49a5f19735e8daee4
StoilTomanov <stoiltomanov@gmail.com>
2024-10-03 00:53:26 +0300

    troubleshhot

--- client/address-book-ui/karma.conf.js
+++ client/address-book-ui/karma.conf.js
@@ -5,6 +5,7 @@ module.exports = function (config) {
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
+            require('karma-json-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
         reporters: ['progress', 'kjhtml', 'json'],
