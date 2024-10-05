e3b61b60324140efeea3b156630e0ea12183986f
StoilTomanov <stoiltomanov@gmail.com>
2024-10-05 16:19:49 +0300

    add coverage report job

--- client/address-book-ui/karma.conf.js
+++ client/address-book-ui/karma.conf.js
@@ -6,10 +6,16 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
+            require('karma-coverage'),
             require('karma-jasmine-html-reporter'),
             require('@angular-devkit/build-angular/plugins/karma'),
         ],
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'kjhtml', 'coverage'],
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage/address-book-ui'),
+            subdir: '.',
+            reporters: [{ type: 'html' }, { type: 'text-summary' }],
+        },
         browsers: ['Chrome'],
         singleRun: false,
     });
