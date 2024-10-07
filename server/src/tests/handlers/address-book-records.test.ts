8c18ace7d77168f84d2858c91707610c0a466fc9
StoilTomanov <stoiltomanov@gmail.com>
2024-10-07 22:54:39 +0300

    add unit tests for backend handlers

--- server/src/tests/handlers/address-book-records.test.ts
+++ server/src/tests/handlers/address-book-records.test.ts
@@ -17,9 +17,17 @@ jest.mock('../../controllers/address-book-records', () => ({
 
 describe('Address book records handler', () => {
     beforeEach(() => {
-        // Explicitly cast to jest.Mock
         (addressBookRecordsController.getAddressBookRecords as jest.Mock).mockImplementation((req, res) => {
             res.status(200).json(dummyAddressBookRecords);
+        });
+        (addressBookRecordsController.createAddressBookRecord as jest.Mock).mockImplementation((req, res) => {
+            res.status(201).json(dummyAddressBookRecords[0]);
+        });
+        (addressBookRecordsController.updateAddressBookRecord as jest.Mock).mockImplementation((req, res) => {
+            res.status(200).json(dummyAddressBookRecords[1]);
+        });
+        (addressBookRecordsController.deleteAddressBookRecord as jest.Mock).mockImplementation((req, res) => {
+            res.status(200).json(dummyAddressBookRecords[2]);
         });
     });
 
@@ -29,9 +37,29 @@ describe('Address book records handler', () => {
 
     it('should call getAddressBookRecords controller when GET /address-book is hit', async () => {
         const response = await request(app).get('/address-book');
-
         expect(response.status).toEqual(200);
         expect(addressBookRecordsController.getAddressBookRecords).toHaveBeenCalledTimes(1);
         expect(response.body).toEqual(dummyAddressBookRecords);
+    });
+
+    it('should call getAddressBookRecords controller when POST /address-book/create is hit', async () => {
+        const response = await request(app).post('/address-book/create');
+        expect(response.status).toEqual(201);
+        expect(addressBookRecordsController.createAddressBookRecord).toHaveBeenCalledTimes(1);
+        expect(response.body).toEqual(dummyAddressBookRecords[0]);
+    });
+
+    it('should call getAddressBookRecords controller when POST /address-book/update/:id is hit', async () => {
+        const response = await request(app).put('/address-book/update/123');
+        expect(response.status).toEqual(200);
+        expect(addressBookRecordsController.updateAddressBookRecord).toHaveBeenCalledTimes(1);
+        expect(response.body).toEqual(dummyAddressBookRecords[1]);
+    });
+
+    it('should call getAddressBookRecords controller when POST /address-book/delete/:id is hit', async () => {
+        const response = await request(app).delete('/address-book/delete/123');
+        expect(response.status).toEqual(200);
+        expect(addressBookRecordsController.deleteAddressBookRecord).toHaveBeenCalledTimes(1);
+        expect(response.body).toEqual(dummyAddressBookRecords[2]);
     });
 });
