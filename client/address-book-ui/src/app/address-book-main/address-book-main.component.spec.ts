3c4f27a3e88a548a4bc9f9067d1b1434f24d9307
StoilTomanov <stoiltomanov@gmail.com>
2024-12-23 23:02:24 +0200

    Add unit tests

--- client/address-book-ui/src/app/address-book-main/address-book-main.component.spec.ts
+++ client/address-book-ui/src/app/address-book-main/address-book-main.component.spec.ts
@@ -1,6 +1,7 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AgGridModule } from 'ag-grid-angular';
+import { GridApi } from 'ag-grid-community';
 
 import { AddressBookMainComponent } from './address-book-main.component';
 import { AddressBookService } from '../services/address-book.service';
@@ -11,6 +12,8 @@ describe('AddressBookMainComponent', () => {
     let component: AddressBookMainComponent;
     let fixture: ComponentFixture<AddressBookMainComponent>;
 
+    let gridApiMock: GridApi;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [AddressBookMainComponent, CcSimpleInputComponent, CcButtonComponent],
@@ -19,10 +22,46 @@ describe('AddressBookMainComponent', () => {
         });
         fixture = TestBed.createComponent(AddressBookMainComponent);
         component = fixture.componentInstance;
+        gridApiMock = jasmine.createSpyObj('GridApi', ['setGridOption']);
+        component.gridApi = gridApiMock;
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    describe('When search text box value change', () => {
+        beforeEach(() => {
+            component.onSearchTextBoxChanged('test');
+        });
+
+        it('it updates the searchValue property', () => {
+            expect(component.searchValue).toEqual('test');
+        });
+
+        it('it calls setGridOption from the grid api', () => {
+            expect(gridApiMock.setGridOption).toHaveBeenCalledOnceWith('quickFilterText', 'test');
+        });
+    });
+
+    describe('When search text box value gets cleared', () => {
+        beforeEach(() => {
+            spyOn(component, 'onSearchTextBoxChanged').and.callThrough();
+            component.searchValue = 'test again';
+            component.onClearSearchInputClicked();
+        });
+
+        it('it updates the searchValue property', () => {
+            expect(component.searchValue).toEqual('');
+        });
+
+        it('it calls onSearchTextBoxChanged', () => {
+            expect(component.onSearchTextBoxChanged).toHaveBeenCalledWith('');
+        });
+
+        it('it calls setGridOption from the grid api', () => {
+            expect(gridApiMock.setGridOption).toHaveBeenCalledOnceWith('quickFilterText', '');
+        });
     });
 });
